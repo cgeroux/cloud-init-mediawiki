76b06383344de44b05a5a7d9048b607a7b2951eb
Chris Geroux <chris.m.geroux@gmail.com>
2016-07-28 15:06:17 +0000

    Fixed up command to run CL install of wiki

--- mediawiki-setup.py
+++ mediawiki-setup.py
@@ -25,23 +25,50 @@ def replaceStrInFile(strMatch,strReplace,fileName):
   file=open(fileName,mode='w')
   file.write(fileText)
   file.close()
-def setupMediaWiki(version="1.27",patch=".0",tmpDir="/tmp"
-  ,documentRoot="/var/www/html",owner="root",group="root",cleanUp=True
-  ,purgeDocRoot=True):
+def setupMediaWiki(settings={}):
   """Downloads media wiki and puts it into document root
   """
   
-  tmpMediaWikiDir=os.path.join(tmpDir,"mediawiki-"+version+patch)
-  url="https://releases.wikimedia.org/mediawiki/"+version+"/mediawiki-" \
-    +version+patch+".tar.gz"
+  defaultSettings={
+    "version":"1.27"
+    ,"patch":"0"
+    ,"wikiName":"Test Wiki"
+    ,"wikiAdminName":"admin"
+    ,"wikiAdminPass":"adminpass123"
+    ,"server":"http://206.167.181.71"
+    ,"dbserver":"localhost"
+    ,"dbuser":"root"
+    ,"dbpass":""
+    ,"documentRoot":"/var/www/html"
+    ,"tmpDir":"/tmp"    
+    ,"owner":"root"
+    ,"group":"root"
+    ,"cleanUp":True
+    ,"purgeDocRoot":True
+    }
+  
+  #set settings to default if no settings given or if setting is None
+  for key in defaultSettings.keys():
+    
+    if key not in settings.keys():
+      settings[key]=defaultSettings[key]
+    else:
+      if settings[key]==None:
+        settings[key]=defaultSettings[key]
+  
+  tmpMediaWikiDir=os.path.join(settings["tmpDir"],"mediawiki-"
+    +settings["version"]+"."+settings["patch"])
+  url="https://releases.wikimedia.org/mediawiki/"+settings["version"] \
+    +"/mediawiki-"+settings["version"]+"."+settings["patch"]+".tar.gz"
   
   #download and untar mediawiki
-  subprocess.call(["wget",url,"--directory-prefix="+tmpDir])
-  subprocess.call(["tar","-xzf",tmpMediaWikiDir+".tar.gz","-C",tmpDir])
+  subprocess.call(["wget",url,"--directory-prefix="+settings["tmpDir"]])
+  subprocess.call(["tar","-xzf",tmpMediaWikiDir+".tar.gz","-C"
+    ,settings["tmpDir"]])
   
   #remove existing files in document root
   if purgeDocRoot:
-    paths=glob.glob(documentRoot+"/*")
+    paths=glob.glob(settings["documentRoot"]+"/*")
     for path in paths:
       try:
         os.remove(path)
@@ -52,27 +79,33 @@ def setupMediaWiki(version="1.27",patch=".0",tmpDir="/tmp"
   paths=glob.glob(tmpMediaWikiDir+"/*")
   for path in paths:
     pathBaseName=os.path.basename(path)
-    shutil.move(path,os.path.join(documentRoot,pathBaseName))
+    shutil.move(path,os.path.join(settings["documentRoot"],pathBaseName))
   
   #change owner and group
   for path in paths:
     pathBaseName=os.path.basename(path)
-    shutil.chown(os.path.join(documentRoot,pathBaseName)
-      ,user=owner
-      ,group=group)
+    shutil.chown(os.path.join(settings["documentRoot"],pathBaseName)
+      ,user=settings["owner
+      ,group=settings["group)
   
   #clean up temporary files
-  if cleanUp:
+  if settings["cleanUp:
     os.removedirs(tmpMediaWikiDir)
     os.remove(tmpMediaWikiDir+".tar.gz")
   
   #do basic configure of the wiki
   #TODO: need to make many of these arguments parameters, and need 
   #to test this
-  subprocess.call(["php","/var/www/html/maintenance/install.php"
-    ,"--scriptpath", "\"\'","--pass","adminpass123","--server"
-    ,"http://206.167.181.71","--dbuser","root","--dbserver", "localhost",
-    "Test Wiki", "adminuser"])
+  subprocess.call(["php"
+    ,os.path.join(settings["documentRoot"],"maintenance/install.php")
+    ,"--scriptpath", "\"\""
+    ,"--pass",settings["wikiAdminPass"]
+    ,"--server",settings["server"]
+    ,"--dbuser",settings["dbuser"]
+    ,"--dbpass",settings["dbpass"]
+    ,"--dbserver",settings["dbserver"]
+    ,settings["wikiName"]
+    ,settings["wikiAdminName"]])
 def restartApache():
   """Restarts apache2
   """
@@ -83,6 +116,6 @@ def main():
   #parse command line options
   (options,args)=parseOptions()
   
-  setupMediaWiki(cleanUp=True)
+  setupMediaWiki()
 if __name__ == "__main__":
  main()
