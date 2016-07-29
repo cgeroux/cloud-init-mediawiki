06b128abf198cc2fa7d742aac0bcc7eb6bf494cb
Chris Geroux <chris.m.geroux@gmail.com>
2016-07-29 21:12:35 +0000

    Added command line options

--- mediawiki-setup.py
+++ mediawiki-setup.py
@@ -16,7 +16,46 @@ def parseOptions():
   parser=op.OptionParser(usage="Usage %prog"
     ,version="%prog 1.0",description="Sets up mediawiki")
   
-  #parse command line options
+  parser.add_option("--dry-run",dest="dryRun",action="store_true",default=False
+    ,help="If set will not actually do anything, only print out what it would "
+    +"have done [not default]")
+  parser.add_option("--wiki-version",dest="version",action="store"
+    ,type="string",default="1.27"
+    ,help="Set the mediawiki version [default: %default].")
+  parser.add_option("--wiki-patch",dest="patch",action="store"
+    ,type="string",default="0"
+    ,help="Set the mediawiki patch version [default: %default].")
+  parser.add_option("--name",dest="wikiName",action="store"
+    ,type="string",default="CC User Wiki"
+    ,help="Sets the name of the wiki [default: %default].")
+  parser.add_option("--read-permission",dest="wikiReadPerm",action="store"
+    ,type="choice",choices=["public","user","sysop"],default="user"
+    ,help="Sets who can read the wiki pages with the exception of the login "
+    +"page [default: %default].")
+  parser.add_option("--edit-permission",dest="wikiEditPerm",action="store"
+    ,type="choice",choices=["public","user","sysop"],default="user"
+    ,help="Sets who can edit the wiki pages [default: %default].")
+  parser.add_option("--account-create-permission",dest="wikiAccCreatePerm"
+    ,action="store",type="choice",choices=["public","user","sysop"]
+    ,default="sysop",help="Sets who can create user accounts the wiki pages "
+    +"[default: %default].")
+  parser.add_option("--admin-user",dest="wikiAdminName",action="store"
+    ,type="string",default=None
+    ,help="Sets wiki administrator user name, by default it is randomly "
+    +"generated.")
+  parser.add_option("--server",dest="server",action="store"
+    ,type="string",default="http://127.0.0.1"
+    ,help="Base url for the server [default: %default]")
+  parser.add_option("--uploads",dest="enableUploads",action="store_true"
+    ,default=False
+    ,help="Enables file uploads [not default]")
+  parser.add_option("--log-url",dest="logoURL",action="store"
+    ,default="$wgResourceBasePath/resources/assets/cc-cloud-wiki-logo.png"
+    ,help="Set the url for the wiki logo [default: %default]")
+  parser.add_option("--extra-config",dest="extraConfigLines",action="append"
+    ,help="Set extra configuration line by providing a string string for "
+    +"LocalSettings.php. Can be used multiple times to include multiple extra "
+    +"configuration lines.")
   return parser.parse_args()
 def replaceStrInFile(strMatch,strReplace,fileName,maxOccurs=None):
   """Replace all occurrences of strMatch with strReplace in file fileName
@@ -87,7 +126,27 @@ def genNameAndPass(length=16
     passwd+=random.SystemRandom().choice(chars)
   
   return (name,passwd)
-def setupMediaWiki(settings={}):
+def execute(func,*args,dry=False,**kwargs):
+  if not dry:
+    func(*args)
+  else:
+    commandStr=func.__name__+"("
+    firstArg=True
+    for arg in args:
+      if firstArg:
+        commandStr+=str(arg)
+        firstArg=False
+      else:
+        commandStr+=","+str(arg)
+    for key in kwargs:
+      if firstArg:
+        commandStr+=key+"="+str(kwargs[key])
+        firstArg=False
+      else:
+        commandStr+=","+key+"="+str(kwargs[key])
+    commandStr+=")"
+    print(commandStr)
+def setupMediaWiki(settings={},dry=False):
   """Downloads media wiki and puts it into document root
   """
   
@@ -104,6 +163,9 @@ def setupMediaWiki(settings={}):
     ,"wikiAdminName":adminName
     ,"wikiAdminPass":adminPassWd
     ,"server":"http://206.167.181.71"
+    ,"enableUploads":False
+    ,"logoURL":"$wgResourceBasePath/resources/assets/cc-cloud-wiki-logo.png"
+    ,"extraConfigLines":[]
     ,"dbserver":"localhost"
     ,"dbuser":"root"
     ,"dbpass":dbPassWd
@@ -113,9 +175,6 @@ def setupMediaWiki(settings={}):
     ,"group":"www-data"
     ,"cleanUp":True
     ,"purgeDocRoot":True
-    ,"enableUploads":False
-    ,"logoURL":"$wgResourceBasePath/resources/assets/cc-cloud-wiki-logo.png"
-    ,"extraConfigLines":[]
     }
   
   #set settings to default if no settings given or if setting is None
@@ -133,42 +192,44 @@ def setupMediaWiki(settings={}):
     +"/mediawiki-"+settings["version"]+"."+settings["patch"]+".tar.gz"
   
   #download and untar mediawiki
-  subprocess.call(["wget",url,"--directory-prefix="+settings["tmpDir"]])
-  subprocess.call(["tar","-xzf",tmpMediaWikiDir+".tar.gz","-C"
-    ,settings["tmpDir"]])
+  execute(subprocess.call,["wget",url,"--directory-prefix="
+    +settings["tmpDir"]],dry=dry)
+  execute(subprocess.call,["tar","-xzf",tmpMediaWikiDir+".tar.gz","-C"
+    ,settings["tmpDir"]],dry=dry)
   
   #remove existing files in document root
   if settings["purgeDocRoot"]:
     paths=glob.glob(settings["documentRoot"]+"/*")
     for path in paths:
       try:
-        os.remove(path)
+        execute(os.remove,path,dry=dry)
       except IsADirectoryError:
-        shutil.rmtree(path)
+        execute(shutil.rmtree,path,dry=dry)
   
   #move files to document root
   paths=glob.glob(tmpMediaWikiDir+"/*")
   for path in paths:
     pathBaseName=os.path.basename(path)
-    shutil.move(path,os.path.join(settings["documentRoot"],pathBaseName))
+    execute(shutil.move,path,os.path.join(settings["documentRoot"]
+      ,pathBaseName),dry=dry)
   
   #change owner and group
   for path in paths:
     pathBaseName=os.path.basename(path)
-    shutil.chown(os.path.join(settings["documentRoot"],pathBaseName)
+    execute(shutil.chown,os.path.join(settings["documentRoot"],pathBaseName)
       ,user=settings["owner"]
-      ,group=settings["group"])
+      ,group=settings["group"],dry=dry)
   
   #clean up temporary files
   if settings["cleanUp"]:
-    os.removedirs(tmpMediaWikiDir)
-    os.remove(tmpMediaWikiDir+".tar.gz")
+    execute(os.removedirs,tmpMediaWikiDir,dry=dry)
+    execute(os.remove,tmpMediaWikiDir+".tar.gz",dry=dry)
   
   #set mysql root password (initial install has no root password)
-  subprocess.call(["mysqladmin","-u","root","password",settings["dbpass"]])
+  execute(subprocess.call,["mysqladmin","-u","root","password",settings["dbpass"]],dry=dry)
   
   #do basic configure of the wiki
-  subprocess.call(["php"
+  execute(subprocess.call,["php"
     ,os.path.join(settings["documentRoot"],"maintenance/install.php")
     ,"--scriptpath", ""
     ,"--pass",settings["wikiAdminPass"]
@@ -177,78 +238,78 @@ def setupMediaWiki(settings={}):
     ,"--dbpass",settings["dbpass"]
     ,"--dbserver",settings["dbserver"]
     ,settings["wikiName"]
-    ,settings["wikiAdminName"]])
+    ,settings["wikiAdminName"]],dry=dry)
   
   localSettingsFile=os.path.join(settings["documentRoot"],"LocalSettings.php")
   
   #enable file uploads
   if settings["enableUploads"]:
-    replaceStrInFile("$wgEnableUploads = false;","$wgEnableUploads = true;"
-      ,localSettingsFile)
+    execute(replaceStrInFile,"$wgEnableUploads = false;"
+      ,"$wgEnableUploads = true;",localSettingsFile,dry=dry)
   
   #copy default cc-wiki-logo
   src=os.path.join(settings["tmpDir"]
     ,"cloud-init-mediawiki/cc-cloud-wiki-logo.png")
   dest=os.path.join(settings["documentRoot"]
     ,"resources/assets/cc-cloud-wiki-logo.png")
-  shutil.copy(src,dest)
+  execute(shutil.copy,src,dest,dry=dry)
   
   #set logo
-  replaceStrInFile(
+  execute(replaceStrInFile,
     "$wgLogo = \"$wgResourceBasePath/resources/assets/wiki.png\";"
     ,"$wgLogo = \""+settings["logoURL"]+"\";"
-    ,localSettingsFile)
+    ,localSettingsFile,dry=dry)
     
   #disable emails as no email client is configured
-  replaceStrInFile("$wgEnableEmail = true;","$wgEnableEmail = false;"
-    ,localSettingsFile)
-  replaceStrInFile("$wgEnableUserEmail = true;","$wgEnableUserEmail = false;"
-    ,localSettingsFile)
+  execute(replaceStrInFile,"$wgEnableEmail = true;","$wgEnableEmail = false;"
+    ,localSettingsFile,dry=dry)
+  execute(replaceStrInFile,"$wgEnableUserEmail = true;","$wgEnableUserEmail = false;"
+    ,localSettingsFile,dry=dry)
   
   
   #secure LocalSettings.php, only owner needs read access
-  shutil.chown(localSettingsFile,user=settings["owner"]
-    ,group=settings["group"])
-  os.chmod(localSettingsFile,0o400)
+  execute(shutil.chown,localSettingsFile,user=settings["owner"]
+    ,group=settings["group"],dry=dry)
+  execute(os.chmod,localSettingsFile,0o400,dry=dry)
   
   #Set read permissions
   if settings["wikiReadPerm"]=="user" or settings["wikiReadPerm"]=="sysop":
-    appendToFile(["$wgGroupPermissions['*']['read'] = false;\n"]
-      ,localSettingsFile)
+    execute(appendToFile,["$wgGroupPermissions['*']['read'] = false;\n"]
+      ,localSettingsFile,dry=dry)
     if settings["enableUploads"]:
       print("WARNING: read permission not public but uploads are enabled."
         +" The public will still be able to see uploads if they know the "
         +"correct url to go directly to the file.")
   if settings["wikiReadPerm"]=="sysop":
-    appendToFile(["$wgGroupPermissions['user']['read'] = false;\n"]
-      ,localSettingsFile)
+    execute(appendToFile,["$wgGroupPermissions['user']['read'] = false;\n"]
+      ,localSettingsFile,dry=dry)
     
   #ensure the login page is always readable
-  appendToFile(["$wgWhitelistRead = array (\"Special:Userlogin\");\n"]
-    ,localSettingsFile)
+  execute(appendToFile,["$wgWhitelistRead = array (\"Special:Userlogin\");\n"]
+    ,localSettingsFile,dry=dry)
   
   #Set edit permissions
   if settings["wikiEditPerm"]=="user" or settings["wikiEditPerm"]=="sysop":
-    appendToFile(["$wgGroupPermissions['*']['edit'] = false;\n"]
-      ,localSettingsFile)
+    execute(appendToFile,["$wgGroupPermissions['*']['edit'] = false;\n"]
+      ,localSettingsFile,dry=dry)
   if settings["wikiEditPerm"]=="sysop":
-    appendToFile(["$wgGroupPermissions['user']['edit'] = false;\n"]
-      ,localSettingsFile)
+    execute(appendToFile,["$wgGroupPermissions['user']['edit'] = false;\n"]
+      ,localSettingsFile,dry=dry)
   
   #Set account creation permissions
   if settings["wikiAccCreatePerm"]=="user" \
     or settings["wikiAccCreatePerm"]=="sysop":
-    appendToFile(["$wgGroupPermissions['*']['createaccount'] = false;\n"]
-      ,localSettingsFile)
+    execute(appendToFile,["$wgGroupPermissions['*']['createaccount'] = false;\n"]
+      ,localSettingsFile,dry=dry)
   if settings["wikiAccCreatePerm"]=="sysop":
-    appendToFile(["$wgGroupPermissions['user']['createaccount'] = false;\n"]
-      ,localSettingsFile)
+    execute(appendToFile,["$wgGroupPermissions['user']['createaccount'] = false;\n"]
+      ,localSettingsFile,dry=dry)
   
   #add any extra configuration options explicitly set
-  appendToFile(settings["extraConfigLines"],localSettingsFile)
+  execute(appendToFile,settings["extraConfigLines"],localSettingsFile,dry=dry)
   
   return (settings["wikiAdminName"],settings["wikiAdminPass"],settings)
-def securePHP():
+def securePHP(dry=False):
   """Ensures some basic php security settings are set
   """
   
@@ -257,38 +318,41 @@ def securePHP():
     "(?<!([^\s]))register_globals[\s]*=[\s]*((O|o)n|(O|o)ff)"
     ,"register_globals = Off","/etc/php5/apache2/php.ini")
   if numReplaces==0:#if no strings replaced add it
-    appendToFile("register_globals = Off\n","/etc/php5/apache2/php.ini")
+    execute(appendToFile,"register_globals = Off\n","/etc/php5/apache2/php.ini"
+      ,dry=dry)
   
   #disable allow_url_fopen
   numReplaces=replaceStrInFileRe(
     "(?<!([^\s]))allow_url_fopen[\s]*=[\s]*((O|o)n|(O|o)ff)"
     ,"allow_url_fopen = Off","/etc/php5/apache2/php.ini")
   if numReplaces==0:#if no strings replaced add it
-    appendToFile("allow_url_fopen = Off\n","/etc/php5/apache2/php.ini")
+    execute(appendToFile,"allow_url_fopen = Off\n","/etc/php5/apache2/php.ini"
+      ,dry=dry)
   
   #ensure session.use_trans_sid is off
   numReplaces=replaceStrInFileRe(
     "(?<!([^\s]))session.use_trans_sid[\s]*=[\s]*[0-1]"
     ,"session.use_trans_sid = 0","/etc/php5/apache2/php.ini")
   if numReplaces==0:#if no strings replaced add it
-    appendToFile("session.use_trans_sid = 0\n","/etc/php5/apache2/php.ini")
+    execute(appendToFile,"session.use_trans_sid = 0\n"
+      ,"/etc/php5/apache2/php.ini",dry=dry)
   
   #restart apache for settings to take effect
-  restartApache()
-def secureMySQL():
+  execute(restartApache,dry=dry)
+def secureMySQL(dry=False):
   """Ensures some basic MySQL security settings are set
   """
   
   #is default for mysql on ubuntu 14.0.4
   #bind-address            = 127.0.0.1
   pass
-def secureApache(documentRoot):
+def secureApache(documentRoot,dry=False):
   """
   """
   
   #disallow any execution of files in the uploads directory
   uploadDirSettings=(
-    "<Directory \""+os.path.join(documentRoot,"/images")+"\"/>\n"
+    "<Directory "+os.path.join(documentRoot,"images/")+">\n"
     "# Ignore .htaccess files\n"
     "AllowOverride None\n"
     "\n"
@@ -300,8 +364,8 @@ def secureApache(documentRoot):
     "\n"
     "# If you've other scripting languages, disable them too.\n"
     "</Directory>\n")
-  appendToFile(uploadDirSettings,"/etc/apache2/apache2.conf")
-  restartApache()
+  execute(appendToFile,uploadDirSettings,"/etc/apache2/apache2.conf",dry=dry)
+  execute(restartApache,dry=dry)
 def restartApache():
   """Restarts apache2
   """
@@ -312,17 +376,32 @@ def main():
   #parse command line options
   (options,args)=parseOptions()
   
+  #map options onto settings
+  dryRun=options.dryRun
+  settings={}
+  settings["version"]=options.version
+  settings["patch"]=options.patch
+  settings["wikiName"]=options.wikiName
+  settings["wikiReadPerm"]=options.wikiReadPerm
+  settings["wikiEditPerm"]=options.wikiEditPerm
+  settings["wikiAccCreatePerm"]=options.wikiAccCreatePerm
+  settings["wikiAdminName"]=options.wikiAdminName
+  settings["server"]=options.server
+  settings["enableUploads"]=options.enableUploads
+  settings["extraConfigLines"]=options.extraConfigLines
+  
+  
   #adjust some php settings to improve security
-  securePHP()
+  securePHP(dry=dryRun)
   
   #adjust some mysql settings to improve security
-  secureMySQL()
+  secureMySQL(dry=dryRun)
   
-  (adminUser,adminPassWd,settings)=setupMediaWiki(
-    settings={"enableUploads":True})
+  (adminUser,adminPassWd,settingsUsed)=setupMediaWiki(
+    settings=settings,dry=dryRun)
   
   #adjust some apache settings to improve security
-  secureApache(settings["documentRoot"])
+  secureApache(settingsUsed["documentRoot"],dry=dryRun)
   
   print("Wiki Admin Username:"+adminUser)
   print("Wiki Admin password:"+adminPassWd)
